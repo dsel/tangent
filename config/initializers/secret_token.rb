266f2f19cd238b1a18847a0d5c24d60d425ad844
Dustin <dselman@gmail.com>
2014-03-13 13:43:27 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tangent::Application.config.secret_key_base = '2c6e69a269f65a781dd91eb6dc67964f67505f92a414d4d38783fc2134325acfb4aab6fe63aabab16517e4834dcb995e375bc0182ba3893b77af76061ff01044'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tangent::Application.config.session_store :cookie_store, key: '_tangent_session'
